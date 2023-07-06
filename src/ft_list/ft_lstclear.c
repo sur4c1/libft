c6f5aa9dc817f64ce09df90fb9cc96524ee14079
Ikar <bguyot@student.42mulhouse.fr>
2023-07-06 09:52:54 +0200

    add nullable del to ft_lstclear

--- src/ft_list/ft_lstclear.c
+++ src/ft_list/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:22:19 by bguyot            #+#    #+#             */
-/*   Updated: 2022/12/01 16:57:13 by bguyot           ###   ########.fr       */
+/*   Updated: 2023/07/06 09:52:21 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if (!lst || !(*lst) || !del)
+	if (!lst || !(*lst))
 		return ;
 	if ((*lst)->next)
 		ft_lstclear(&(*lst)->next, del);
-	del((*lst)->content);
+	if (del)
+		del((*lst)->content);
 	free(*lst);
 	*lst = NULL;
 }
