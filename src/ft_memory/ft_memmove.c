47d4fac67be2b40c074da0b6ec60478bd57cdbd3
Ikar Azphæl <94838879+laird-ikar@users.noreply.github.com>
2023-11-07 19:20:48 +0100

    fix memmove unsigned >= 0 test

--- src/ft_memory/ft_memmove.c
+++ src/ft_memory/ft_memmove.c
@@ -28,7 +28,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 			i = len - 1;
 			step = -1;
 		}
-		while (i >= 0 && i < len)
+		while (i < len)
 		{
 			((char *) dst)[i] = ((char *) src)[i];
 			i += step;
