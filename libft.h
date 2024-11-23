12c804d514df8fa99441f9db4a3d4e2c860aace5
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2024-11-23 16:28:47 +0100

    🚨 Compiling with another makefile

--- libft.h
+++ libft.h
@@ -6,24 +6,24 @@
 /*   By: yyyyyy <yyyyyy@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:24:47 by bguyot            #+#    #+#             */
-/*   Updated: 2024/11/21 13:00:17 by yyyyyy           ###   ########.fr       */
+/*   Updated: 2024/11/23 16:15:31 by yyyyyy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include "ft_arr.h"
-# include "ft_cast.h"
-# include "ft_check.h"
-# include "ft_gnl.h"
-# include "ft_math.h"
-# include "ft_memory.h"
-# include "ft_put.h"
-# include "ft_string.h"
-# include "ft_list.h"
-# include "ft_buff.h"
-# include "ft_printf.h"
-# include "ft_getopt.h"
+# include "incs/ft_arr.h"
+# include "incs/ft_cast.h"
+# include "incs/ft_check.h"
+# include "incs/ft_gnl.h"
+# include "incs/ft_math.h"
+# include "incs/ft_memory.h"
+# include "incs/ft_put.h"
+# include "incs/ft_string.h"
+# include "incs/ft_list.h"
+# include "incs/ft_buff.h"
+# include "incs/ft_printf.h"
+# include "incs/ft_getopt.h"
 
 #endif
