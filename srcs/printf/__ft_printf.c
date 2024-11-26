278ad2ac7c2e039cd3a780c2b579102dee6f4430
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2024-11-26 06:29:35 -0500

    🚨 still broken tho

--- srcs/printf/__ft_printf.c
+++ srcs/printf/__ft_printf.c
@@ -6,35 +6,22 @@
 /*   By: yyyyyy <yyyyyy@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:11:54 by yyyyyy            #+#    #+#             */
-/*   Updated: 2024/11/26 06:04:54 by yyyyyy           ###   ########.fr       */
+/*   Updated: 2024/11/26 06:12:27 by yyyyyy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "__ft_printf.h"
 
 static
-size_t	parse_argindex(char *format, int *arg_index, int current_index)
-{
-	size_t	len;
-
-	len = 0;
-	while (ft_isdigit(format[len]))
-	{
-		*arg_index = *arg_index * 10 + format[len] - '0';
-		len++;
-	}
-}
-
-static
 size_t	parse_arg(char *format, va_list ap, t_buff *buff)
 {
-	t_conversion	conv;
 	size_t			len;
 
 	len = 1;
-	conv = (t_conversion){0};
-	len += parse_argindex(format + len, &conv.arg_index, 0);
 	return (len);
+	(void) ap;
+	(void) buff;
+	(void) format;
 }
 
 static
