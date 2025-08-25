2349ae84dddda4ee341d410e71aa6ad8519cea0f
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2025-08-25 16:50:18 +0200

    Yeeting useless tests

--- test/gnl/test.sh
+++
@@ -1,2 +0,0 @@
-cc gnl_test.c ../../libft.a -o gnl_test &> /dev/null
-./gnl_test gnl_test.c
--- test/test.sh
+++
@@ -1,4 +0,0 @@
-for file in test/**/test.sh; do
-	echo "Testing" $(dirname $file | cut -d'/' -f2)
-	(cd $(dirname $file) && bash ./test.sh)
-done
